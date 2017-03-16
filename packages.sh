5d6cb46de2f581915b96fd373209835d0aba7d40
Nami San <nami@Namis-Mac.local>
2017-03-16 12:22:37 -0400

    init

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@ dpkg-deb -bZgzip projects/AppSyncUnified debs
 dpkg-deb -bZgzip projects/LoveIsAgain debs
 dpkg-deb -bZgzip projects/Down debs
 dpkg-deb -bZgzip projects/Failsafe debs
-dpkg-deb -bZgzip projects/IAPCrazy debs
+dpkg-deb -bZgzip projects/Extendlife debs
 dpkg-deb -bZgzip projects/KGPayphone debs
 dpkg-deb -bZgzip projects/LocalIAPStore debs
 dpkg-deb -bZgzip projects/TweakModeNC debs
@@ -10,3 +10,5 @@ dpkg-deb -bZgzip projects/Hayah debs
 dpkg-deb -bZgzip projects/MJKaman debs
 dpkg-deb -bZgzip projects/iOS6 debs
 dpkg-deb -bZgzip projects/LobsterTwo debs
+
+DEBIA
